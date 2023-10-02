8484dea83f7a0bc76648ab8d850b6aea3159d0eb
Dav Jodhun <116104373+DJodhun@users.noreply.github.com>
2023-10-02 13:09:37 +0100

    Add files via upload

---
+++ PHYS4038_Satellite_Tracking_in_Python_with_Streamlit.py
@@ -0,0 +1,290 @@
+import streamlit as st
+from skyfield.api import Loader, EarthSatellite
+from skyfield.timelib import Time
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import urllib.request 
+from urllib.request import urlopen
+from datetime import datetime as dt #To tidy up the date and time import for Celestrak
+#The following are for the TEME Coordinates from TLE Data
+from sgp4.api import Satrec
+from sgp4.api import SGP4_ERRORS
+from astropy.time import Time
+from astropy.coordinates import TEME, CartesianDifferential, CartesianRepresentation
+from astropy import units as u
+from astropy.coordinates import ITRS
+from astropy.coordinates import EarthLocation
+import cartopy.crs as ccrs
+import matplotlib.cm as mplcm
+import matplotlib.colors as colours
+from skyfield import api
+from pytz import timezone
+import plotly as py
+import plotly.tools as tls
+
+#streamlit run PHYS4038_Satellite_Tracking_in_Python_with_Streamlit.py
+
+def axislimits(axis):
+    """
+    Generates the 3D axes in which the orbits are to be plotted in. Takes 
+    the axes as the argument, which is defined when plotting. 
+    
+    """
+    try:
+        lims = axis.get_xlim(), axis.get_ylim(), axis.get_zlim()
+    except AttributeError:
+        pass
+    try:
+        centres = [0.5*sum(pair) for pair in lims] 
+        widths  = [pair[1] - pair[0] for pair in lims]
+        halfwidth = 0.5*max(widths)
+        axis.set_xlim(centres[0] - halfwidth, centres[0] + halfwidth)
+        axis.set_ylim(centres[1] - halfwidth, centres[1] + halfwidth)
+        axis.set_zlim(centres[2] - halfwidth, centres[2] + halfwidth)
+        return centres, halfwidth
+    except UnboundLocalError:
+        pass
+
+def dataacquisition(url):
+    """
+    Takes data from the Celestrak webpage, which comes as Two Line Element
+    (TLE) data, in which position, velocity and time are encoded. This function
+    then decodes the TLE data into utf-8 from bytes, and then splits the 
+    2 lines into 2 lists, ready for use later on. 
+    
+    """
+    source = urllib.request.urlopen(url).readlines()
+    source = [s.decode("utf-8") for s in source]
+    source = [s.strip() for s in source]
+    names = source[0::3]
+    del source[0::3]
+    L1list, L2list = source[0::2], source[1::2]
+    L1list = [s.strip() for s in L1list]
+    L2list = [s.strip() for s in L2list]
+    length = len(names)
+    return L1list, L2list, names, length
+
+def earthMesh(ax, lonspace, latspace):
+    """
+    Plots a mesh sphere, of radius 6378 (km), using an array of latitude and 
+    longitude lines. This sphere represents Earth, and will most likely be 
+    replaced or covered by an image - currently testing plotly to plot Earth 
+    on the surface.
+    
+    """
+    theta = np.linspace(0, twopi, 201)
+    cth, sth, zth = [f(theta) for f in (np.cos, np.sin, np.zeros_like)]
+    lon0 = re*np.vstack((cth, zth, sth))
+    lons = []
+    lat0 = re*np.vstack((cth, sth, zth))
+    lats = []
+    for phi in rads*lonspace:
+        cph, sph = [f(phi) for f in (np.cos, np.sin)]
+        lon = np.vstack((lon0[0]*cph - lon0[1]*sph, lon0[1]*cph + lon0[0]*sph, lon0[2]))
+        lons.append(lon)
+    for phi in rads*latspace:
+        cph, sph = [f(phi) for f in (np.cos, np.sin)]
+        lat = re*np.vstack((cth*cph, sth*cph, zth+sph))
+        lats.append(lat)
+    for x, y, z in lons:
+        ax.plot(x, y, z, '-k')
+    for x, y, z in lats:
+        ax.plot(x, y, z, '-k')
+    return lons, lats
+
+def GPS_orbit_plotter(ax, time, length):
+    """
+    Plots the orbits of the GPS satellites, using the decoded TLE data. The 
+    Skyfield library handles these data and combines it with the time in order
+    to calculate the orbits. A second scatter plot is overlaid on top of the 
+    first, to provide the locations of the satellites at a given time. 
+    
+    """
+    GPSL = []
+    GPSPL = []
+    GPSPL2 = []
+    for i in range(length):
+        GPS = EarthSatellite(L1list[i], L2list[i])
+        GPSL.append(GPS)
+#        print(GPS.epoch.tt)
+        GPSpos = GPS.at(time).position.km
+        GPSPL.append(GPSpos)
+        GPSpos2 = GPS.at(time2).position.km
+        GPSPL2.append(GPSpos2)
+        print(GPSpos)
+#        print(GPSpos.shape)
+        if True:    
+#            fig = plt.figure(figsize=[10, 8])  # [12, 10] for alternate size. 
+#            ax  = fig.add_subplot(1, 1, 1, projection='3d') #These lines are here for individual plots of each orbit, should they be needed.
+            x, y, z = GPSpos
+            a, b, c = GPSpos2
+#            markers_on = [a,b,c]
+            ax.plot(x, y, z)#, markevery = markers_on, marker = "o")
+            ax.scatter(a, b, c)
+            ax.text(x[i],y[i],z[i], '%s' % (str(names[i])), size=10, zorder=1, color='k')            
+        centres, halfwidth = axislimits(ax)
+        #plt.legend(names, bbox_to_anchor=(1.05, 1))
+#    plt.show()
+    return GPS, GPSpos
+    
+def orbitalElementPlotter3D(time, length):
+    """
+    Plots the Earth mesh and the orbits in one single plot, and saves it to use
+    in Streamlit. Unfortunately, saved plot cannot be interacted with, yet, in 
+    the Streamlit environment.
+    
+    """
+    fig1 = plt.figure(figsize=[10, 8])  # [12, 10]
+    ax  = fig1.add_subplot(1, 1, 1, projection='3d')
+    NUM_COLOURS = 31
+    cm = plt.get_cmap('jet')
+    cNorm  = colours.Normalize(vmin=0, vmax=NUM_COLOURS-1)
+    scalarMap = mplcm.ScalarMappable(norm=cNorm, cmap=cm)
+    ax.set_prop_cycle(color=[scalarMap.to_rgba(i) for i in range(NUM_COLOURS)])
+    earthMesh(ax, lonspace, latspace)
+    GPS_orbit_plotter(ax, time, length)
+#    plt.show()
+#    plt.savefig('OEP3D.png')
+#    return ax
+
+def orbitalElementPlotter2D(time, length):
+    """
+    Plots the ground traces of the satellite orbits on the Plate Carree world
+    map projection, along with a point to show the location of the satellite
+    at the given time. 
+
+    """
+    fig2 = plt.figure(figsize=[12, 10])  # [12, 10]
+    ax = fig2.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+    NUM_COLOURS = 31
+    cm = plt.get_cmap('jet') #gist_rainbow/rainbow
+    cNorm  = colours.Normalize(vmin=0, vmax=NUM_COLOURS-1)
+    scalarMap = mplcm.ScalarMappable(norm=cNorm, cmap=cm)
+    ax.set_prop_cycle(color=[scalarMap.to_rgba(i) for i in range(NUM_COLOURS)])
+    ax.coastlines()
+    for i in range(length):
+        GPS = EarthSatellite(L1list[i], L2list[i])
+        GPSgeocentric = GPS.at(time)
+        GPSgeocentric2 = GPS.at(time2)
+        subsat = GPSgeocentric.subpoint()
+        subsat2 = GPSgeocentric2.subpoint()
+        if True:
+            plt.scatter(subsat.longitude.degrees, subsat.latitude.degrees, transform=ccrs.PlateCarree(), linewidth=1, s=0.6)
+            plt.scatter(subsat2.longitude.degrees, subsat2.latitude.degrees)
+#            ax.text(x[i],y[i], '%s' % (str(names[i])), size=10, zorder=1, color='k')        
+            plt.legend(names, bbox_to_anchor=(1.05, 1))            
+    plt.show()
+
+def overhead_polar_plot(overhead_pass):
+    """
+    
+    This is a proof of concept plot, that, given more time and better coding
+    expertise, would show the traces of the satellites in the sky, looking
+    upwards, given a location on Earth. Currently, only the first GPS satellite
+    in the TLE data is used to plot the figure. The for loop would have been used
+    to plot the rest, in a similar fashion as before, however it would result 
+    in a very cluttered figure. This part of the code was modified from 
+    https://rhodesmill.org/brandon/2018/tiangong/, which is part of the 
+    Skyfield API. 
+    
+    """
+    i, j = overhead_pass
+    fig3 = plt.figure()
+    ax = plt.subplot(111, projection="polar")
+    ax.set_rlim([0, 90])
+    ax.set_theta_zero_location('N')
+    ax.set_theta_direction(-1)
+    theta = az.radians
+    r = 90 - alt.degrees
+    ax.plot(theta, r, '--')
+    for k in range(len(theta)):
+        text = time[k].astimezone(utc).strftime('%H:%M')
+#    plt.show()
+    
+#%%
+#SKYFIELD FOR TLE TO ORBITAL POSITION
+
+url = "http://celestrak.org/NORAD/elements/gp.php?GROUP=gps-ops&FORMAT=tle"
+L1list, L2list, names, length = dataacquisition(url)
+
+#Defining useful data for Skyfield and constants
+halfpi, pi, twopi = [f*np.pi for f in (0.5, 1, 2)]
+degs, rads = 180/pi, pi/180
+lonspace = np.arange(0, 180, 15)
+latspace = np.arange(-75, 105, 15)
+load = Loader('~/Documents/fishing/SkyData')
+data = load('de421.bsp')
+ts   = load.timescale()
+planets = load('de421.bsp')
+earth   = planets['earth']
+re = 6378.
+
+#Defining times
+hours = np.arange(0, 24, 0.01) #For a 24 hour period. Points on the 2D map are generated using hours = 0
+time = ts.utc(2022, 12, 18, hours)
+time2 = ts.utc(2022, 12, 18, 0)
+
+#%%
+
+#Plotting (Without Streamlit)
+orbitalElementPlotter3D(time, length)
+orbitalElementPlotter2D(time, length)
+
+utc = timezone('UTC')
+earthloc = api.Topos(latitude='52.9388 N', longitude='1.1981 W') #UoN location
+for i in range(length):  
+    GPS = EarthSatellite(L1list[0], L2list[0])
+    orbitval = (GPS - earthloc).at(time)
+    alt, az, distance = orbitval.altaz()
+    above_horizon = alt.degrees > 0
+    boundaries, = np.diff(above_horizon).nonzero()
+    passes = boundaries.reshape(len(boundaries) // 2, 2)
+
+opp = overhead_polar_plot(passes[0])
+
+#%%
+#STREAMLIT PLOTS
+
+st.write("# Satellite Plotting in Python")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+option = st.selectbox(
+    "Select a set of orbital elements",
+    ("GPS Satellites", "GLONASS Satellites", "Space Stations", "Starlink (WIP)", "Military Satellites"))
+
+ISS = "ISS (ZARYA) 1 25544U 98067A   22353.76579140  .00011344  00000+0  20600-3 0  9997 2 25544  51.6432 137.2564 0003570 175.9484 324.6865 15.50061212373969"
+
+if option == "GPS Satellites":
+    url = "http://celestrak.org/NORAD/elements/gp.php?GROUP=gps-ops&FORMAT=tle"
+elif option == "Space Stations":
+    url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle"
+elif option == "Military Satellites":
+    url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=military&FORMAT=tle"
+elif option == "GLONASS Satellites":
+    url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=glo-ops&FORMAT=tle"
+elif option == "Debris (IRIDIUM 33)":
+    url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=iridium-33-debris&FORMAT=tle"
+else:
+    option
+
+L1list, L2list, names, length = dataacquisition(url)
+
+
+# placeholder = st.empty()
+# st3Dplot = orbitalElementPlotter3D(time, length)
+# #placeholder.pyplot(orbitalElementPlotter3D(time, length))
+# st.pyplot(st3Dplot)
+
+# st.pyplot(orbitalElementPlotter2D(time, length))
+
+# st.pyplot(opp)
+
+placeholder = st.empty()
+# st3Dplot = orbitalElementPlotter3D()
+placeholder.pyplot(orbitalElementPlotter3D(time, length))
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.pyplot(orbitalElementPlotter2D(time, length))
+
+st.pyplot(opp)
--- README.md
+++ README.md
@@ -1,2 +1,112 @@
-# RTST
-Real Time Satellite Tracking with Python
+# Scientific Programming in Python – Submission 3
+
+### Project title: Real-time GPS Satellite Tracking
+
+### Student name: Davlesh Jodhun
+### Course: MSc Machine Learning in Science
+### University: University of Nottingham
+
+## Background
+There are 31 Global Positioning System satellites in Medium Earth Orbit (about 20,200km above Earth). Being in a precise location at a given time is of crucial importance to their use in navigation. In total, there are about 5,500 satellites in orbit, with a projceted number of 58,000 by 2030. The problem of satellite tracking is not novel, since the various governments have had to track them since their inception. Likewise, the problem of coding it within Python is not new either, having been done before. Skyfield is a package that can be used to faciliate the project. See https://github.com/xtai/Real-Time-Satellite-Tracking/blob/master/3d-refactor-2.py for an example done without Skyfield. 
+
+This project will track the position of GPS satellites in orbit over Earth and visualise their paths over a 3D model of Earth. Then, (based on the feedback received from the previous submission) satellites will be plotted over a map using Cartopy, with a view to use Streamlit to select locations and see what satellites are overhead. Since Skyfield is being used, the project includes satellites that are in orbit, and the code allows for these different sets of satellites to be plotted. 
+
+GPS TLE data will be taken from the website http://celestrak.org/NORAD/elements/gp.php?GROUP=gps-ops&FORMAT=tle and stored at reasonable intervals to have the data offline. The TLE data becomes unfit for predicting orbits 2 weeks either side of the date the data was gathered, so it is important that the data is refreshed regularly and for comparisons, the data is kept for later. The TLE data contains information such as the position, name, position and time, alongside other metrics beloning to the satellite, all encoded to two lines. Data for other satellites can be taken from this website as well.
+
+## Outline 
+#### List of modules to be used (initial): 
+  - matplotlib: https://matplotlib.org/stable/index.html
+  - scipy: https://scipy.org/
+  - numpy: https://numpy.org/
+  - pandas: https://pandas.pydata.org/
+  - Cartopy: https://scitools.org.uk/cartopy/docs/latest/
+  - Skyfield and sgp4: https://rhodesmill.org/skyfield/
+  - Astropy: https://www.astropy.org/
+  - Modules such as sched and time to keep taking data over time: https://docs.python.org/3/library/sched.html
+  - Streamlit: https://streamlit.io/
+
+#### Structure of code:
+  - Defined functions and classes in a separate file, to be called elsewhere. 
+  - Main file: setup of data and plotting the axes and the sphere, importing the other functions. 
+  - Extra folder for 3D assets, if they are to be used and overlaid over the mesh sphere. 
+
+#### Potentially several user defined functions:
+  - One function to pull the data from the website and store it, updating at regular intervals that make sense, such as every 10-30 seconds.
+  - One function to display the Earth/Mercator Projection Map (if there is no exisiting one that can do this) on a set of axes. Alternatively, just drawing a sphere in a set of axes can work if there is time trouble. 
+  - One function to plot the paths of the satellites, plotting the past data as solid lines, and future data in dashed lines.
+  - One function to find the patches of sky where there are the fewest satellites passing through in a given time period. 
+
+## Code Snippets
+
+Data Acquisition:
+
+```
+import urllib.request 
+from urllib.request import urlopen
+from skyfield.api import Loader, EarthSatellite
+
+url = "http://celestrak.org/NORAD/elements/gp.php?GROUP=gps-ops&FORMAT=tle"
+
+def dataacquisition(url):
+    source = source = urllib.request.urlopen(url).readlines()
+    source = [s.decode("utf-8") for s in source]
+    source = [s.strip() for s in source]
+    names = source[0::3]
+    del source[0::3]
+    L1list, L2list = source[0::2], source[1::2]
+    L1list = [s.strip() for s in L1list]
+    L2list = [s.strip() for s in L2list]
+    return L1list, L2list, names
+
+L1list, L2list, names = dataacquisition(url)            #This function takes the raw TLE data, decodes it, then separates it into name and the 2 lines of data
+
+```
+The dataacquisition function requires a method that means it can run at regular intervals to ensure that the TLE data is accurate for the time being. This will be done using sched or time, or a combination of both. 
+
+## Pseudocode
+
+```
+##Plotting the Earth as a wire sphere:
+#radiusE = 6378  #rE in km
+#longitude_lines = [] #Need to choose a number of lines of longitude such that the sphere is visible - so fewer than 360
+#latitude_lines = [] #There are 180 lines of latitude
+
+#class Satellite:
+#    def __init__(self, name, L1, L2)       #Reading the split TLE data - can be done in Skyfield with more ease. 
+#    
+#    3D position vector with parameters (x, y, z) relative to the radius of the Earth
+#    
+#    Define the Satellite as a square on the grid, that will draw out the orbit 
+
+#Following the definition of the class of satellites, the following can be executed with Skyfield:
+#
+#L1List, L2List = TLE.load()      #This is not a necessary line of code, but can be used to get a handle on the TLE data. 
+#
+# for i in range(len(names)):
+#     sat = names[i], L1list[i], L2list[i]
+#     satpos = sat.at(time).position.km       #This loop can position the satellites in the right places, such that their orbits can be drawn.
+#
+
+```
+Defining Satellite as a class will be the basis of creating all the satellites from the TLE data. For now, the plotting element has been kept to a function for each set of satellites, and might not require the use of classes. 
+
+## Previous uncertainties, from submission 1: 
+  - Not sure if this is particularly feasible for my level of programming. Finding the patches of sky that have minimal satellite coverage is going to be difficult, espcially if I am aiming for a graphical output. 
+  - The Skyfield library (in particular the module sgp4) is used to carry out this kind of task. While the use of Skyfield might seem to trivialise the project, it presents its own challenges.
+  
+## Previous goals, from submission 2:
+  - The code, as of right now, takes the data from Celestrak (only GPS data for now, might expand this later), and plots their orbits, along with a wire sphere representing Earth. Included is the current plot output from the TLE data in 3D space.
+  - The next step will be to finish converting the coordinates into usable Cartesian coordinates to be plotted on a map of Earth, using Cartopy, to show the paths the satellites take. This should not take too much longer. I will also try and see if I can wrap the same map on to the sphere. 
+  - Another small tweak would be to use datetime alongside skyfield such that the current date and time can be used. This should also not take too long. 
+  - Creating a Streamlit dashboard (as per the feedback) is currently in the works, as I am getting to grips with how it works behind the scenes.
+  - I will look to make the program more efficient in the final touches. 
+
+## Final Submission
+  - The code now takes data for several Celestrak pages, including GPS, GLONASS, Space Stations and Military (Miscellaneous). The orbits are plotted in 3D, with the same Earth mesh, and the ground tracks are plotted in 2D. The Polar plot, looking outwards from the coordinates for the University of Nottingham, shows the path traced by the first GPS satellite on the list. 
+  - A Streamlit dashboard has been created. The line "streamlit run PHYS4038_Satellite_Tracking_in_Python_with_Streamlit.py" pasted into the anaconda navigator should bring up the dashboard, for now. I will put it on a public repository, if required. 
+  - Final plots have been provided as images. These images use the most up to date Celestrak data (03:00 on 21/12/2022) to predict where the GPS satellites will be at 00:00 on 25/12/2022. 
+ 
+## Further Improvements
+  - Due to time constraints, the final plot - the Polar plot only shows the path of the first GPS satellite when looking outwards. 
+  - If I had the time and expertise, I would use a for loop to plot all the possible versions of the plot, separately (this is fleshed out in the report). Additionally, wrapping the Earth mesh in a map was not completed, due to the same constraints, and issues with the PIL library. 
+  - I will probably tweak parts of this project in my spare time (if I get any) next semester. 
